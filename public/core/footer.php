2a3184b2b32710a3963ef2d7a8932011daeded85
Rick <144334803+8-Click@users.noreply.github.com>
2024-05-27 10:46:36 +0200

    Effe rap
    Ga ff gauw pushen

--- public/core/footer.php
+++ public/core/footer.php
@@ -2,4 +2,5 @@
 
 <p>Copyright (&copy; 2023-<?= Date("Y") ?> rileydeman | &copy; 2024-<?= Date("Y") ?> 8click)</p>
 
-<a href="https://www.zoekeenleven.be">Contact ons</a>
+<a id="contactLink" href="https://www.zoekeenleven.be" target="_BLANK">Contact</a>
+<a id="aboutusLink" href="https://www.zoekeenleven.be" target="_BLANK">Over ons</a>
