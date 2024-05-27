02c06649d7bfbfb2be0cc24c93101eba2dc363fa
Rick <144334803+8-Click@users.noreply.github.com>
2024-05-27 11:42:22 +0200

    Footer klaar joehoe jaja WOOOO
    SUPER COOL

--- public/core/footer.php
+++ public/core/footer.php
@@ -1,13 +1,17 @@
 <!--Create the footer without the footer tags-->
 
-<p>COPYRIGHT (&copy; 2023-<?= Date("Y") ?> rileydeman | &copy; 2024-<?= Date("Y") ?> 8click)</p>
+<p>&copy; 2023-<?= Date("Y") ?> rileydeman | &copy; 2024-<?= Date("Y") ?> 8click</p>
 
 <a id="contactLink" href="https://www.zoekeenleven.be" target="_BLANK">CONTACT</a>
-<a id="aboutusLink" href="https://www.zoekeenleven.be" target="_BLANK">OVER ONS</a>
+
 
 <div id="vraagKnop">
-    <img src="./public/assets/img/question.png" alt="questionMark" widt="100" height="100" style="object-fit:contain;">
+    <img src="./public/assets/img/previous.png" alt="previousIcon" width="50" height="50" style="object-fit:contain;">
+    <img src="./public/assets/img/question.png" alt="questionMark" width="100" height="100" style="object-fit:contain;">
+    <img src="./public/assets/img/next.png" alt="nextIcon" width="50" height="50" style="object-fit:contain;">
 </div>
 
+<a id="aboutusLink" href="https://www.zoekeenleven.be" target="_BLANK">OVER ONS</a>
 
-<img id="logo" src="./public/assets/img/Autologo.png" alt="logo" width="300" height="300" style="object-fit:contain;margin-left: 200px;">
+
+<img id="logo" src="./public/assets/img/Autologo.png" alt="logo" width="200" height="200" style="object-fit:contain;margin-left: 200px;">
