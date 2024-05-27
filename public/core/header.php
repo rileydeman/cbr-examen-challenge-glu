31c03c4b55ca336aaee024a66029e9d1cfac6e38
Riley de Man <rileydeman@gmail.com>
2024-05-27 11:46:09 +0200

    Header logo and timer (static) ready
    Header logo and timer (static) ready

--- public/core/header.php
+++ public/core/header.php
@@ -1,2 +1,11 @@
-<!--Create the header without the header tags-->
-<p>Header 123</p>
+<div id="logoDiv">
+    <a href="/" id="logo">
+        <img src="/public/assets/img/cbr-logo.png" alt="">
+    </a>
+</div>
+<div id="timerDiv">
+    <div id="timerCircle">
+        <p id="timerText">69</p>
+    </div>
+</div>
+<div id="buttonsDiv"></div>
