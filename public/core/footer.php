f1bffca75974828f8e914163da86ef11869500b1
Rick <144334803+8-Click@users.noreply.github.com>
2024-05-27 10:01:48 +0200

    SUPER COOLE EERSTE FOOTER JAJAJAJAJAJJAJAJA

--- public/core/footer.php
+++ public/core/footer.php
@@ -1,3 +1,5 @@
 <!--Create the footer without the footer tags-->
 
 <p>Copyright (&copy; 2023-<?= Date("Y") ?> rileydeman | &copy; 2024-<?= Date("Y") ?> 8click)</p>
+
+<a href="https://www.zoekeenleven.be">Contact ons</a>
