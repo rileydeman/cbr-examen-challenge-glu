0798c955c2c8dc787d5b1d47882790f1cfce15c6
Rick <144334803+8-Click@users.noreply.github.com>
2024-05-27 11:25:52 +0200

    Super coole footer push
    Super coole footer push naar de branch footer JAJAJAJAJJA WHAHHAAJAHAA

--- public/core/footer.php
+++ public/core/footer.php
@@ -1,6 +1,13 @@
 <!--Create the footer without the footer tags-->
 
-<p>Copyright (&copy; 2023-<?= Date("Y") ?> rileydeman | &copy; 2024-<?= Date("Y") ?> 8click)</p>
+<p>COPYRIGHT (&copy; 2023-<?= Date("Y") ?> rileydeman | &copy; 2024-<?= Date("Y") ?> 8click)</p>
 
-<a id="contactLink" href="https://www.zoekeenleven.be" target="_BLANK">Contact</a>
-<a id="aboutusLink" href="https://www.zoekeenleven.be" target="_BLANK">Over ons</a>
+<a id="contactLink" href="https://www.zoekeenleven.be" target="_BLANK">CONTACT</a>
+<a id="aboutusLink" href="https://www.zoekeenleven.be" target="_BLANK">OVER ONS</a>
+
+<div id="vraagKnop">
+    <img src="./public/assets/img/question.png" alt="questionMark" widt="100" height="100" style="object-fit:contain;">
+</div>
+
+
+<img id="logo" src="./public/assets/img/Autologo.png" alt="logo" width="300" height="300" style="object-fit:contain;margin-left: 200px;">
