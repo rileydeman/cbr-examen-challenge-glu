9dba329fd38710af0420f580ce844c49f92e9aee
Rick <144334803+8-Click@users.noreply.github.com>
2024-05-29 11:03:13 +0200

    Start test knop zit erin

--- public/home.php
+++ public/home.php
@@ -3,37 +3,44 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HOME | CBR examen rijbewijs B</title>
 
     <link rel="icon" href="">
 
-<!--    Linking of the css files-->
-    <link rel="stylesheet" href="/public/assets/css/fonts.css">
-    <link rel="stylesheet" href="/public/assets/css/style.css">
-    <link rel="stylesheet" href="/public/assets/css/header.css">
-    <link rel="stylesheet" href="/public/assets/css/footer.css">
+    <!--    Linking of the css files-->
+    <link rel="stylesheet" href="./public/assets/css/fonts.css">
+    <link rel="stylesheet" href="./public/assets/css/style.css">
+    <link rel="stylesheet" href="./public/assets/css/header.css">
+    <link rel="stylesheet" href="./public/assets/css/footer.css">
+    <link rel="stylesheet" href="./public/assets/css/home.css">
 
-<!--    Linking of the javascript files, defer = reading/link the file at last-->
-    <script defer src="/public/assets/js/app.js"></script>
+    <!--    Linking of the javascript files, defer = reading/link the file at last-->
+    <script defer src="./public/assets/js/app.js"></script>
 </head>
 
 <body>
 
-<header>
-<!--    Getting the header document-->
-    <?php include "public/core/header.php" ?>
-</header>
+    <header>
+        <!--    Getting the header document-->
+        <?php include "public/core/header.php" ?>
+    </header>
 
-<main>
-    <p>Main</p>
-</main>
+    <main>
+        <div id="startTestContainer">
+            <p id="startTestTekst">START TEST</p>
+            <a href="public/test.php">
+                <img id="startButton" src="./public/assets/img/video.png">
+            </a>
+        </div>
+    </main>
 
-<footer>
-<!--    Getting the footer document-->
-    <?php include "public/core/footer.php" ?>
-</footer>
+    <footer>
+        <!--    Getting the footer document-->
+        <?php include "public/core/footer.php" ?>
+    </footer>
 
 </body>
 
