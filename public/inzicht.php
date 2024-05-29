24bbcbcbf199b63ea272ed06f3786b409810e8c2
Riley de Man <rileydeman@gmail.com>
2024-05-29 12:44:51 +0200

    Bug fix pages won't load
    Bug fix pages won't load

---
+++ public/inzicht.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en-AU">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inzicht | CBR examen rijbewijs B</title>
+
+    <link rel="icon" href="">
+
+    <!--    Linking of the css files-->
+    <link rel="stylesheet" href="./public/assets/css/fonts.css">
+    <link rel="stylesheet" href="./public/assets/css/style.css">
+    <link rel="stylesheet" href="./public/assets/css/header.css">
+    <link rel="stylesheet" href="./public/assets/css/footer.css">
+    <link rel="stylesheet" href="./public/assets/css/inzicht.css">
+
+    <!--    Linking of the javascript files, defer = reading/link the file at last-->
+    <script defer src="./public/assets/js/app.js"></script>
+    <script defer src="./public/assets/js/home.js"></script>
+</head>
+
+<body>
+
+<header>
+    <!--    Getting the header document-->
+    <?php include "core/header.php" ?>
+</header>
+
+<main>
+    <h1>Inzicht</h1>
+</main>
+
+<footer>
+    <!--    Getting the footer document-->
+    <?php include "core/footer.php" ?>
+</footer>
+
+</body>
+
+</html>
