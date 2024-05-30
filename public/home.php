283ce205be751c9d67f485a6248aa97d51b32359
Riley de Man <rileydeman@gmail.com>
2024-05-30 11:52:02 +0200

    Fixed issue in home.php
    Fixed issue in home.php

--- public/home.php
+++ public/home.php
@@ -30,7 +30,7 @@
     </header>
 
     <main>
-        <form action="../app/controllers/create-exam.php" target="_self" method="post" id="startForm">
+        <form action="./app/controllers/create-exam.php" target="_self" method="post" id="startForm">
             <img id="cbrLogo" src="./public/assets/img/cbr-logo.png" alt="">
             <h1>theorie examen rijbewijs B (auto)</h1>
             <p>Welkom bij uw rijbewijs B theorie examen, vul hieronder uw gegevens in en klik daarna op "Start Examen".</p>
