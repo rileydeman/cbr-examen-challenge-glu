d85007cab635befd8fe0e6966ec1eca6b2fc2d23
Rick <144334803+8-Click@users.noreply.github.com>
2024-05-31 09:52:49 +0200

    uifygdsufgfdgsfu9uhshehgdfliufhsdihgtrewiolqshwduiftdhrfleohvbcgfiolgufdsuikhoigffsc

--- public/kennis.php
+++ public/kennis.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kennis | CBR examen rijbewijs B</title>
 
@@ -24,19 +24,48 @@
 
 <body>
 
-<header>
-    <!--    Getting the header document-->
-    <?php include "core/header.php" ?>
-</header>
+    <header>
+        <!--    Getting the header document-->
+        <?php include "core/header.php" ?>
+    </header>
 
-<main>
-    <h1>Kennis</h1>
-</main>
+    <main>
+        <div id="quizBox">
+            <div id="questionBox">
+                <p>
+                    WAAR of NIET WAAR: Als je op de weg rijdt moet je uit je doppen kijken
+                </p>
+            </div>
+            <div id="deRest">
+                <div id="pictureBox">
+                    <img id="quizPicture" src="./public/assets/img/placeholder.png">
+                </div>
+            </div>
+            <br>
+            <br>
+            <div id="answersBox">
+                <form action="../app/controllers/progress-gevaarherkenning-question.php" method="post" id="choicesBox">
 
-<footer>
-    <!--    Getting the footer document-->
-    <?php include "core/footer.php" ?>
-</footer>
+                    <label for="option1radio" class="options" id="option1">
+                        <div class="option">1.</div>
+                        <div class="optionTekst">Remmen</div>
+                        <input type="radio" name="answer" id="option1radio" value="0">
+                    </label>
+                    <label for="option2radio" class="options" id="option2">
+                        <div class="option">2.</div>
+                        <div class="optionTekst">Gas loslaten</div>
+                        <input type="radio" name="answer" id="option2radio" value="1">
+                    </label>
+                    <input type="hidden" name="questionNumber" value="">
+                </form>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <!--    Getting the footer document-->
+        <?php include "core/footer.php" ?>
+    </footer>
 
 </body>
 
